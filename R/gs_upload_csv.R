514d60327928ffa8dc632912d91842802caf5f83
patelm9 <patelm9@mskcc.org>
2019-10-07 17:31:21 -0400

    + functions

--- R/gs_upload_csv.R
+++ R/gs_upload_csv.R
@@ -1,16 +1,14 @@
 #' Write to new Google Sheet
 #' @param csv_filename full path to file to upload
 #' @param new_gs_name name of new gs
-#' @param rm_Sheet1 should Sheet1 be deleted?
 #' @param system.sleep time in seconds for system sleep
 #' @importFrom googlesheets gs_upload
 #' @export
 
 gs_upload_csv <-
-        function(csv_filename, new_gs_name, system.sleep) {
+        function(csv_filename, new_gs_name, system.sleep = 0.3) {
                 gsheet_metadata <- googlesheets::gs_upload(file = csv_filename,
                                         sheet_title = new_gs_name)
-
-                return(gsheet_metadata$sheet_key)
                 Sys.sleep(system.sleep)
+                return(gsheet_metadata$sheet_key)
         }
