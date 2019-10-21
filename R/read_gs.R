c590dab115bdc7604c0ebd3029d416e04c6d4093
patelm9 <patelm9@mskcc.org>
2019-10-21 11:06:32 -0400

    +: retrieve_gsheet_id_by_name function

--- R/read_gs.R
+++ R/read_gs.R
@@ -1,9 +1,12 @@
 #' Reads Google Sheet as a list of dataframes or a single dataframe
 #' @param gsheet_metadata gsheet_metadata
-#' @importFrom googlesheets gs_read_csv
+#' @import googlesheets
 #' @export
+#'
+
 read_gs <-
         function(gsheet_metadata, system.sleep = 2) {
+
 
                 if (gsheet_metadata$n_ws > 1L) {
                         x <- list()
@@ -12,7 +15,7 @@ read_gs <-
                                 googlesheets::gs_read_csv(gsheet_metadata,
                                                           ws = i,
                                                           check.names = TRUE,
-                                                          col_types = cols(.default = "c"))
+                                                          col_types = cols(.default = col_character()))
                                 Sys.sleep(system.sleep)
                         }
                         names(x) <- tabnames_gs(gsheet_metadata)
@@ -22,7 +25,7 @@ read_gs <-
                         googlesheets::gs_read_csv(gsheet_metadata,
                                                   ws = 1,
                                                   check.names = TRUE,
-                                                  col_types = cols(.default = "c")
+                                                  col_types = cols(col_character())
                                                   )
                         return(x)
                 }
---
+++ R/retrieve_gsheet_id_by_name.R
@@ -0,0 +1,19 @@
+#' Get Google Sheet id by name
+#' @param gsheet_name character string of length one of the exact match
+#' @import googledrive
+#' @import typewriteR
+#' @export
+
+
+retrieve_gsheet_id_by_name <-
+        function(gsheet_name) {
+                dribble <- googledrive::drive_ls(pattern = paste0("^", gsheet_name, "$"),
+                                      type = "spreadsheet")
+                if (nrow(dribble) > 1) {
+                        typewriteR::tell_me("ERROR:", "There are a total of", nrow(dribble), "Google Sheets with the name", gsheet_name, ".")
+                } else {
+                        gsheet_id <- dribble$id
+                        names(gsheet_id) <- gsheet_name
+                        return(gsheet_id)
+                }
+        }
