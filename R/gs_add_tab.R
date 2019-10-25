8a5936f8e25b1fee1fc32bbd32d09f79e43a2dcf
patelm9 <patelm9@mskcc.org>
2019-10-25 18:04:24 -0400

    + gs_add_tab function

---
+++ R/gs_add_tab.R
@@ -0,0 +1,28 @@
+#' Adds a dataframe to a new tab with the given name to the gsheet identified by name
+#' @import googlesheets
+#' @import dplyr
+#' @import typewriteR
+#' @export
+
+gs_add_tab <-
+        function(dataframe, gsheet_name, tab_name) {
+                gsheet_id <- get_gsheet_id_from_name(gsheet_name)
+
+                gs_ss <- googlesheets::gs_ws_new(googlesheets::gs_key(gsheet_id), ws_title = tab_name, input = colnames(dataframe), byrow = TRUE)
+
+                gs_browse(gs_ss, ws = tab_name)
+                typewriteR::tell_me("Please add blank row to the new", tab_name, "and")
+                typewriteR::stop_and_enter()
+
+                for (i in 1:nrow(dataframe)) {
+                        cat("\n")
+                        typewriteR::tell_me(Sys.time(), "Starting row", i, "of total", nrow(dataframe), "rows.")
+                        cat("\t\t")
+                        gs_ss <- googlesheets::gs_add_row(gs_ss, ws = tab_name, input = dataframe %>%
+                                                                                                dplyr::filter(row_number() == i))
+                        cat("\n")
+                        typewriteR::tell_me(Sys.time(), "Finished writing row", i, "of total", nrow(dataframe), "rows.")
+                        cat("\n")
+                        Sys.sleep(4)
+                }
+        }
