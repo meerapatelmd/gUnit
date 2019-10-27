999768ef12f850ed7c6608cfbf42a828cc6e2289
Meera Patel <patelm9@mskcc.org>
2019-10-26 21:03:03 -0400

    bug fix

--- R/gs_add_tab.R
+++ R/gs_add_tab.R
@@ -13,7 +13,7 @@ gs_add_tab <-
 
                 if (!(tab_name %in% tab_names)) {
                         gsheet_metadata <- googlesheets::gs_ws_new(googlesheets::gs_key(gsheet_id), ws_title = tab_name, input = colnames(dataframe), byrow = TRUE)
-                        gs_browse(gs_ss, ws = tab_name)
+                        gs_browse(gsheet_metadata, ws = tab_name)
                         typewriteR::tell_me("Please add blank row to the new", tab_name, "and")
                         typewriteR::stop_and_enter()
                 }
@@ -22,7 +22,7 @@ gs_add_tab <-
                         cat("\n")
                         typewriteR::tell_me(Sys.time(), "Starting row", i, "of total", nrow(dataframe), "rows.")
                         cat("\t\t")
-                        gs_ss <- googlesheets::gs_add_row(gs_ss, ws = tab_name, input = dataframe %>%
+                        gsheet_metadata <- googlesheets::gs_add_row(gsheet_metadata, ws = tab_name, input = dataframe %>%
                                                                                                 dplyr::filter(row_number() == i))
                         cat("\n")
                         typewriteR::tell_me(Sys.time(), "Finished writing row", i, "of total", nrow(dataframe), "rows.")
