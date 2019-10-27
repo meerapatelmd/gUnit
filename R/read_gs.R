a68366f3fd45ad1ca5d6434dba029ea704e4d38b
Meera Patel <patelm9@mskcc.org>
2019-10-26 20:27:40 -0400

    +: functions

--- R/read_gs.R
+++ R/read_gs.R
@@ -1,33 +1,45 @@
 #' Reads Google Sheet as a list of dataframes or a single dataframe
-#' @param gsheet_metadata gsheet_metadata
+#' @param gsheet_name gsheet_name
 #' @import googlesheets
 #' @import somersaulteR
 #' @export
 #'
 
 read_gs <-
-        function(gsheet_metadata, system.sleep = 2) {
+        function(gsheet_name, tab = NULL, system.sleep = 3) {
+                gsheet_metadata <- gs_ss_by_name(gsheet_name)
+                Sys.sleep(system.sleep)
+          if (is.null(tab)) {
+                  if (gsheet_metadata$n_ws > 1L) {
+                          x <- list()
+                          for (i in 1:gsheet_metadata$n_ws) {
+                                  x[[i]] <-
+                                          googlesheets::gs_read_csv(gsheet_metadata,
+                                                                    ws = i,
+                                                                    check.names = TRUE,
+                                                                    col_types = cols(.default = col_character())) %>%
+                                          somersaulteR::call_mr_clean()
+                                  Sys.sleep(system.sleep)
+                          }
+                          names(x) <- tabnames_gs(gsheet_metadata)
+                          return(x)
+                  } else if (gsheet_metadata$n_ws == 1L) {
+                          x <-
+                                  googlesheets::gs_read_csv(gsheet_metadata,
+                                                            ws = 1,
+                                                            check.names = TRUE,
+                                                            col_types = cols(.default = col_character())) %>%
+                                  somersaulteR::call_mr_clean()
+                          return(x)
+                  }
+          } else {
+                  x <- googlesheets::gs_read_csv(gsheet_metadata,
+                                                 ws = tab,
+                                                 check.names = TRUE,
+                                                 col_types = cols(.default = col_character())) %>%
+                         somersaulteR::call_mr_clean()
+                  return(x)
+          }
 
-
-                if (gsheet_metadata$n_ws > 1L) {
-                        x <- list()
-                        for (i in 1:gsheet_metadata$n_ws) {
-                                x[[i]] <-
-                                googlesheets::gs_read_csv(gsheet_metadata,
-                                                          ws = i,
-                                                          check.names = TRUE) %>%
-                                        somersaulteR::call_mr_clean()
-                                Sys.sleep(system.sleep)
-                        }
-                        names(x) <- tabnames_gs(gsheet_metadata)
-                        return(x)
-                } else if (gsheet_metadata$n_ws == 1L) {
-                        x <-
-                        googlesheets::gs_read_csv(gsheet_metadata,
-                                                  ws = 1,
-                                                  check.names = TRUE) %>%
-                                somersaulteR::call_mr_clean()
-                        return(x)
-                }
 
         }
